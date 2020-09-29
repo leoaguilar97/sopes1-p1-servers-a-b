bbdd5d11ecf718eefd4933b69026846548df7630
Leo <leoaguilarsanchez97@gmail.com>
2020-09-29 05:00:35 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -21,25 +21,6 @@ def index():
 
 @application.route('/todo')
 def todo():
-    _todos = db.todo.find()
-
-    item = {}
-    data = []
-    for todo in _todos:
-        item = {
-            'id': str(todo['_id']),
-            'todo': todo['todo']
-        }
-        data.append(item)
-
-    return jsonify(
-        status=True,
-        data=data
-    )
-
-@application.route('/stats')
-def todo():
-
     stats = {
         'cpu_freq_percpu': psutil.cpu_freq(percpu=True),
         'cpu_freq': psutil.cpu_freq(percpu=True),
@@ -58,6 +39,24 @@ def todo():
         stats=stats
     )
 
+@application.route('/stats')
+def stats():
+    _todos = db.todo.find()
+
+    item = {}
+    data = []
+    for todo in _todos:
+        item = {
+            'id': str(todo['_id']),
+            'todo': todo['todo']
+        }
+        data.append(item)
+
+    return jsonify(
+        status=True,
+        data=data
+    )
+
 @application.route('/todo', methods=['POST'])
 def createTodo():
     data = request.get_json(force=True)
@@ -72,6 +71,7 @@ def createTodo():
     ), 201
 
 if __name__ == "__main__":
+    print("INICIANDO API")
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
     ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
     application.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)

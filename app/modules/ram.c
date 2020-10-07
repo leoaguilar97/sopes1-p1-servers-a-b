43f10dddda3b52a0bacb78776437fcfe4016adc9
Leo <leoaguilarsanchez97@gmail.com>
2020-10-06 21:55:30 -0600

    Update ram.c

--- app/modules/ram.c
+++ app/modules/ram.c
@@ -25,7 +25,7 @@ int lru;
 static int show_cpu_percent(struct seq_file *m, void *v){
     #define K(x) ((x) << (PAGE_SHIFT - 10))
     si_meminfo(&i);
-    seq_printf(m,"LIBRE: %8lu TOTAL: %8lu", K(i.freeram), K(i.totalram));   
+    seq_printf(m,"\nRAM:\n FRAM: %8lu\n TRAM: %8lu\n", K(i.freeram), K(i.totalram));   
     return 0;   
 }
 
